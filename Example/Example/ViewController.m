e8df29eb7b3eeade8a72bceb07266cac343a8a4c
wwz2016 <wwz2016@163.com>
2017-03-07 14:28:19 +0800

    add example

---
+++ Example/Example/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Example
+//
+//  Created by wwz on 17/3/7.
+//  Copyright © 2017年 tijio. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WWZESPTouch/WWZESPTouch.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
